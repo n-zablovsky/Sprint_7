2fd2dc16638b3a36eac5283512b67a8f257ae7c0
Nikolai Zablovsky <nickolas.z@yandex.ru>
2025-07-19 12:14:31 +0300

    additions based on comments

--- src/main/java/org/example/courier/Courier.java
+++ src/main/java/org/example/courier/Courier.java
@@ -1,51 +1,21 @@
 package org.example.courier;
 
-
+import lombok.Data;
+import lombok.AllArgsConstructor;
 import java.util.concurrent.ThreadLocalRandom;
 
+@Data
+@AllArgsConstructor
 public class Courier {
     private String login;
     private String password;
     private String firstName;
 
-    public Courier(String login, String password, String firstName) {
-        this.login = login;
-        this.password = password;
-        this.firstName = firstName;
-    }
-
     public Courier() {
-
     }
 
     public static Courier random() {
         int suffix = ThreadLocalRandom.current().nextInt(100, 100_000);
         return new Courier("Petfed" + suffix, "pass1234", "Petfed");
     }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
 }
-
-
